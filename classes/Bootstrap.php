25cf58d724023059210f854c1cc31c31d4d645c3
stefano ricci <stefano.ricci01@outlook.it>
2019-03-31 19:14:31 +0200

    First commit

---
+++ classes/Bootstrap.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stefa
+ * Date: 27/03/2019
+ * Time: 13:14
+ */
+
+class Bootstrap{
+    private $controller;
+    private $action;
+    private $request;
+
+    /**
+     * Bootstrap constructor.
+     * @param $request
+     */
+    public function __construct($request)
+    {
+        $this->request = $request;
+        if ($this->request['controller']==""){
+            $this->controller='home';
+        }else{
+            $this->controller=$this->request['controller'];
+        }
+        if ($this->request['action']==""){
+            $this->action='index';
+        }else{
+            $this->action=$this->request['action'];
+        }
+    }
+
+
+}
